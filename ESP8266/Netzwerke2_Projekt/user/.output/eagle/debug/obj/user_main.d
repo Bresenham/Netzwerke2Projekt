3113ccc27df3786041c2d2f056681a8a6f1e2764
Bresenham <gaul@hm.edu>
2019-05-01 20:49:17 +0200

    Initial Commit

---
+++ ESP8266/Netzwerke2_Projekt/user/.output/eagle/debug/obj/user_main.d
@@ -0,0 +1,23 @@
+.output/eagle/debug/obj/user_main.o .output/eagle/debug/obj/user_main.d : user_main.c /opt/esp/sdk/include/user_interface.h \
+ /opt/esp/sdk/include/os_type.h /opt/esp/sdk/include/ets_sys.h \
+ /opt/esp/sdk/include/c_types.h /opt/esp/sdk/include/eagle_soc.h \
+ /opt/esp/sdk/include/ip_addr.h /opt/esp/sdk/include/queue.h \
+ user_config.h /opt/esp/sdk/include/spi_flash.h \
+ /opt/esp/sdk/include/gpio.h /opt/esp/sdk/include/ets_sys.h \
+ /opt/esp/sdk/include/osapi.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/string.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/xtensa/config/core-isa.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/reent.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/_types.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_types.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_default_types.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/lock.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/cdefs.h \
+ /opt/esp/crosstool-NG/builds/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/string.h
